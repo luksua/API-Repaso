eaf0a4d9b3d9d97933e6b46fbb9bc3ea01770fc3
Luna Ospina <lunasofiaor48@gmail.com>
2025-10-14 10:51:05 -0500

    se sube componente Button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,1 +1,17 @@
-asdad
+import React from "react";
+
+interface ButtonProps {
+    label: string;
+    onClick: () => void;
+    type?: "button" | "submit";
+}
+
+const Button: React.FC<ButtonProps> = ({ label, onClick, type }) => {
+    return (
+        <button onClick={onClick} type={type}>
+            {label}
+        </button>
+    );
+};
+
+export default Button;
